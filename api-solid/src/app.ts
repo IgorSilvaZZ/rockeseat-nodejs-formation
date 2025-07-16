4f88a9538b3663104a7b6114d7dd0fd1e8a5c7b4
IgorSilvaZZ <igor06silva12@gmail.com>
2025-07-16 18:27:00 -0300

    feat: :heavy_plus_sign: add user controller

--- api-solid/src/app.ts
+++ api-solid/src/app.ts
@@ -1,26 +1,7 @@
 import fastify from "fastify";
-import { z } from "zod";
 
-import { prisma } from "./lib/prisma";
+import { appRoutes } from "./http/routes";
 
 export const app = fastify();
 
-app.post("/users", async (req, rep) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.email(),
-    password: z.string().min(6),
-  });
-
-  const { name, email, password } = registerBodySchema.parse(req.body);
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      passwordHash: password,
-    },
-  });
-
-  return rep.status(201).send();
-});
+app.register(appRoutes);
