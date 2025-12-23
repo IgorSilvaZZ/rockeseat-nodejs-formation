8c36601a89f23a19096fd5f705e22209bb6158ca
IgorSilvaZZ <igor06silva12@gmail.com>
2025-12-22 22:51:41 -0300

    feat: :lock: add middleware to verify jwt token

--- api-solid/src/http/controllers/profile.controller.ts
+++ api-solid/src/http/controllers/profile.controller.ts
@@ -1,7 +1,15 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
+import { makeGetUserProfileUseCase } from "../useCases/factories/make-get-user-profile-useCase.factory";
 
 export async function profile(req: FastifyRequest, rep: FastifyReply) {
 	await req.jwtVerify();
 
-	return rep.status(200).send();
+	const getUserProfileUseCase = makeGetUserProfileUseCase();
+
+	const { user } = await getUserProfileUseCase.execute({ id: req.user.sub });
+
+	return rep.status(200).send({
+		...user,
+		password_hash: undefined,
+	});
 }
--- api-solid/src/http/routes.ts
+++ api-solid/src/http/routes.ts
@@ -4,10 +4,12 @@ import { authenticate } from "./controllers/authenticate.controller";
 import { profile } from "./controllers/profile.controller";
 import { register } from "./controllers/register.controller";
 
+import { verifyJWT } from "./middlewares/verify-jwt.middleware";
+
 export async function appRoutes(app: FastifyInstance) {
 	app.post("/users", register);
 	app.post("/sessions", authenticate);
 
 	/* Authenticated routes */
-	app.post("/me", profile);
+	app.post("/me", { onRequest: [verifyJWT] }, profile);
 }
