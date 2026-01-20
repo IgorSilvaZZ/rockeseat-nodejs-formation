2cce3a8f04a8762dd970c12b62971ba58ec98c09
IgorSilvaZZ <igor06silva12@gmail.com>
2026-01-20 08:55:24 -0300

    test: :white_check_mark: adjustment test e2e to role users

--- api-solid/src/utils/test/create-and-authenticate-user.ts
+++ api-solid/src/utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,19 @@
+import { hash } from "bcryptjs";
 import type { FastifyInstance } from "fastify";
 import request from "supertest";
+import { prisma } from "@/lib/prisma";
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-	await request(app.server).post("/users").send({
-		name: "Igor Silva",
-		email: "igor@email.com",
-		password: "senha123",
+export async function createAndAuthenticateUser(
+	app: FastifyInstance,
+	isAdmin = false,
+) {
+	await prisma.user.create({
+		data: {
+			name: "Igor Silva",
+			email: "igor@email.com",
+			passwordHash: await hash("senha123", 6),
+			role: isAdmin ? "ADMIN" : "MEMBER",
+		},
 	});
 
 	const authResponse = await request(app.server).post("/sessions").send({
