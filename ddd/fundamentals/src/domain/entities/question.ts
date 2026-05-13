ab0046750c44f400000b50a4085e220d1ce05167
IgorSilvaZZ <igor06silva12@gmail.com>
2026-05-12 21:22:35 -0300

    feat: :heavy_plus_sign: add getters and setters in entities

--- ddd/fundamentals/src/domain/entities/question.ts
+++ ddd/fundamentals/src/domain/entities/question.ts
@@ -1,6 +1,6 @@
 import { Entity } from "../../core/entities/entity";
 import type { Optional } from "../../core/types/optional";
-import type { Slug } from "./value-objects/slug";
+import { Slug } from "./value-objects/slug";
 import type { UniqueEntityId } from "./value-objects/unique-entity-id";
 
 export interface QuestionProps {
@@ -8,19 +8,74 @@ export interface QuestionProps {
 	content: string; // Conteudo da questão
 	slug: Slug; // Representação do titulo questão sem acentos e caracteres especias
 	authorId: UniqueEntityId; // Autor da questão
-	bestAnswerId?: string; // A melhor resposta para a questão
+	bestAnswerId?: UniqueEntityId; // A melhor resposta para a questão
 	createdAt: Date; // Data de criação
 	updatedAt?: Date; // Data de atualização
 }
 
 export class Question extends Entity<QuestionProps> {
+	get title() {
+		return this.props.title;
+	}
+
+	get content() {
+		return this.props.content;
+	}
+
+	get slug() {
+		return this.props.slug;
+	}
+
+	get authorId() {
+		return this.props.authorId;
+	}
+
+	get bestAnswerId() {
+		return this.props.bestAnswerId;
+	}
+
+	get createdAt() {
+		return this.props.createdAt;
+	}
+
+	get updatedAt() {
+		return this.props.updatedAt;
+	}
+
+	get excerpt() {
+		return this.content.substring(0, 120).trimEnd().concat("...");
+	}
+
+	private touch() {
+		this.props.updatedAt = new Date();
+	}
+
+	set content(content: string) {
+		this.props.content = content;
+
+		this.touch();
+	}
+
+	set title(title: string) {
+		this.props.title = title;
+
+		this.props.slug = Slug.createFromText(title);
+
+		this.touch();
+	}
+
+	set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+		this.props.bestAnswerId = bestAnswerId;
+	}
+
 	static create(
-		props: Optional<QuestionProps, "createdAt">,
+		props: Optional<QuestionProps, "createdAt" | "slug">,
 		id?: UniqueEntityId,
 	): Question {
 		const question = new Question(
 			{
 				...props,
+				slug: props.slug ?? Slug.createFromText(props.title),
 				createdAt: new Date(),
 			},
 			id,
